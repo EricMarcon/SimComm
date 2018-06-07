e563cd54d4cbcae100d02e2f2a9eff16a24cbdf3
Eric Marcon <eric.marcon@ecofog.gf>
2018-06-07 19:01:23 -0300

    Loop instead of sapply

--- R/CG_Models.R
+++ R/CG_Models.R
@@ -11,12 +11,20 @@ cg_drift <- R6Class("cg_drift",
   inherit = community_gridmodel,
   private = list(
     evolve =  function(time, save) {
-      # Find the neighbors of points
-      the_neighbors <- sapply(seq(self$pattern$n), function(point) self$neighbors(point))
-      # Choose a neighbor for each point
-      chosen_neighbors <- apply(the_neighbors, 2,  sample, size=1)
-      # Change the marks
-      self$pattern$marks$PointType <- sapply(chosen_neighbors, function(point) self$pattern$marks$PointType[point])
+      # Prepare the vector of new species
+      the_species <- self$pattern$marks$PointType
+
+      for(point in seq(self$pattern$n)) {
+        # Find the neighbors of the point
+        the_neighbors <- self$neighbors(point)
+        # Choose one
+        chosen_neighbor <- sample(the_neighbors, size=1)
+        # Change the point's mark
+        the_species[point] <- self$pattern$marks$PointType[chosen_neighbor]
+      }
+      # Change the pattern
+      self$pattern$marks$PointType <- the_species
+
       if(save) {
         # Save the new pattern
         self$run_patterns[[which(self$timeline == time)]] <- self$pattern
